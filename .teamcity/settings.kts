fac07dab3be5dbd3121a663c94686687e99ec716
Roman <ramon_st@mall.ru>
2025-06-05 15:35:16 +0300

    mr2

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -24,7 +24,7 @@ object BuildMR : BuildType({
     steps {
         script {
             name = "Print MR Test Message"
-            scriptContent = """echo "HELLO I'M TEST MR""""
+            scriptContent = """echo "HELLO I'M TEST MR-2""""
         }
     }
 
